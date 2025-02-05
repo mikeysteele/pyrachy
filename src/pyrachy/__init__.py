0fb44a805c779721158dd84655f5c06501137af9
Michael Westcott <mjwestcott@gmail.com>
2025-02-06 09:57:55 +1100

    refactor: add proper module exports

--- src/pyrachy/__init__.py
+++ src/pyrachy/__init__.py
@@ -1,6 +1,11 @@
-from .pyrachy import Pyrachy  # type: ignore
-from .loaders.dict_loader import DictLoader  # type: ignore
-from .loaders.env_loader import EnvLoader  # type: ignore
-from .loaders.argv_loader import ArgvLoader  # type: ignore
-from .loaders.file_loader import FileLoader  # type: ignore
-from .loaders.base_loader import BaseLoader  # type: ignore
+from .loaders import BaseLoader, ArgvLoader, EnvLoader, DictLoader, FileLoader
+from .pyrachy import Pyrachy
+
+__all__ = [
+    "Pyrachy",
+    "DictLoader",
+    "EnvLoader",
+    "ArgvLoader",
+    "FileLoader",
+    "BaseLoader",
+]
--- src/pyrachy/loaders/__init__.py
+++ src/pyrachy/loaders/__init__.py
@@ -1,5 +1,7 @@
-from src.pyrachy.loaders.dict_loader import DictLoader  # type: ignore
-from src.pyrachy.loaders.env_loader import EnvLoader  # type: ignore
-from src.pyrachy.loaders.argv_loader import ArgvLoader  # type: ignore
-from src.pyrachy.loaders.file_loader import FileLoader  # type: ignore
-from src.pyrachy.loaders.base_loader import BaseLoader  # type: ignore
+from .argv_loader import ArgvLoader
+from .base_loader import BaseLoader
+from .dict_loader import DictLoader
+from .env_loader import EnvLoader
+from .file_loader import FileLoader
+
+__all__ = ["DictLoader", "EnvLoader", "ArgvLoader", "FileLoader", "BaseLoader"]
